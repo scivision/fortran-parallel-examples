9649d9cd4a2a307211c744b9374999c19f113ae5
scivision <scivision@users.noreply.github.com>
2024-12-18 10:02:53 -0500

    do concurrent reduce: explain/print/check values

--- do_concurrent/do_concurrent_reduction.f90
+++ do_concurrent/do_concurrent_reduction.f90
@@ -1,20 +1,36 @@
 program do_concurrent_reduction
 !! code from https://wg5-fortran.org/N2151-N2200/N2194.pdf
 
-implicit none
+!! In this example, the output values of `a` and `b` will be:
+!! `xsqs`: The sum of the squares of the random values in `x`.
+!! `mx`: The maximum value in the array `x`.
+use, intrinsic :: iso_fortran_env, only: stderr=>error_unit
+
+implicit none (type, external)
 
 integer, parameter :: n = 1000000
 
-real :: a, b
+real :: xsqs, mx
 real, allocatable :: x(:)
 integer :: i
 
 allocate(x(n))
 
-do concurrent (i = 1: n) reduce(+:a) reduce(max:b)
-  a = a + x(i)**2
-  b = max(b,x(i))
+! Initialize x with random values between 0 and 1
+call random_seed()
+call random_number(x)
+
+do concurrent (i = 1: n) reduce(+:xsqs) reduce(max:mx)
+  xsqs = xsqs + x(i)**2
+  mx = max(mx, x(i))
 end do
 
+deallocate(x)
+
+print *, xsqs, mx
+
+if (mx < 0 .or. mx > 1) then
+  write(stderr, '(a)') 'Error: mx is out of range [0, 1]'
+end if
 
 end program
